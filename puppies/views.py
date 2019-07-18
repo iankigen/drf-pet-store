8ef0c5640c51987b935c8bc4363cfb51f224f47d
Ian Kigen <ian.Gabe.ian@gmail.com>
2019-07-18 18:36:41 +0300

    chore: add viewset

--- puppies/views.py
+++ puppies/views.py
@@ -1,6 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import SearchFilter
 
-# Create your views here.
+from .models import Puppies
+from .serializers import PuppiesSerializer
+from .paginantion import PuppiesPaginantion
+
+
+class PuppiesViewSet(ModelViewSet):
+	queryset = Puppies.objects.all()
+	serializer_class = PuppiesSerializer
+	permission_classes = []
+	http_method_names = ('get', 'post', 'put', 'delete')
+	filter_backends = [SearchFilter]
+	search_fields = ('name', 'color', 'breed', 'age')
+	pagination_class = PuppiesPaginantion
