865b626fa2dff62cb4ca85252cba33afbcf82048
Ian Kigen <ian.Gabe.ian@gmail.com>
2019-07-18 18:37:21 +0300

    chore: register admin site

--- puppies/admin.py
+++ puppies/admin.py
@@ -3,4 +3,14 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from .models import Puppies
+
+
+class PuppiesAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name', 'color', 'breed', 'created_at')
+	list_filter = ('breed', 'color', 'name')
+	ordering = ('name', 'color', 'id', 'breed')
+	search_fields = ('name', 'color', 'id', 'breed')
+
+
+admin.site.register(Puppies, PuppiesAdmin)
