88ee8aad31d8e65fd11594997e99b764b8b95101
Ian Kigen <ian.Gabe.ian@gmail.com>
2019-07-18 17:08:54 +0300

    chore: add puppies app

---
+++ puppies/models.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+class Puppies(models.Model):
+	name = models.CharField(blank=False, null=False, max_length=255)
+	age = models.IntegerField(default=0)
+	breed = models.CharField(blank=False, null=False, max_length=50)
+	color = models.CharField(blank=False, null=False, max_length=50)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return self.name
